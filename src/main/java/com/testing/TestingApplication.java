3ef7b9d8dfe9c9e8a66876c03297fc7a4b3d75a8
KumarShubham101 <156191373+KumarShubham101@users.noreply.github.com>
2026-04-24 18:13:01 +0530

    Update TestingApplication.java

--- src/main/java/com/testing/TestingApplication.java
+++ src/main/java/com/testing/TestingApplication.java
@@ -5,7 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestingApplication {
-
+		String name="mike";
+		int x=100;
+		boolean isPresent=false;
+		int y=200;
+		char x='a';
+		int y=200;
+		double z=10.3;
+		float m=10.3f;
+		int a=2;
+		boolean isSub=true;
+		char i ='d';
 	public static void main(String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
 	}
